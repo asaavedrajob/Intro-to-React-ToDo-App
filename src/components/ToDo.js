5ebd5c5322dfd522ae0a43c9d3d1da043af6945e
Aldo Diaz <asaavedrajob@gmail.com>
2021-06-25 12:37:00 -0500

    Reset ToDo file name (restore)

---
+++ src/components/ToDo.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import "components/ToDo.css"
+
+export default function ToDo({ doneProp, textProp }) {
+  const doneAsBoolean = doneProp === undefined ? false : doneProp
+
+  const [done, setDone] = useState(doneAsBoolean)
+  const [text, setText] = useState(textProp)
+
+  // (AS) we can use this feature, but the problem is that the item renders without properties and once they are assigned, then the submit is triggered when there is no change on the chks or text inputs
+  useEffect(
+    function () {
+      console.info("useEffect done: Cambio!!", done)
+      handleSubmit()
+    },
+    [done]
+  )
+
+  useEffect(
+    function () {
+      console.info("useEffect text: Cambio!!", text)
+      handleSubmit()
+    },
+    [text]
+  )
+
+  function handleSubmit() {
+    console.log("This is where the submit will happen!", done, text)
+  }
+
+  function handleKeyPress() {
+    if (event.key === "Enter") {
+      handleSubmit()
+    }
+  }
+
+  return (
+    <div className="todo-box display-flex margin-bottom-4">
+      <label>
+        <input
+          type="checkbox"
+          checked={done}
+          onChange={() => {
+            setDone(!done)
+          }}
+        />
+        <span></span>
+      </label>
+      <div className="input-field inline width-100">
+        <input
+          type="text"
+          value={text}
+          placeholder={
+            text === undefined || text === "" ? "Type your ToDo item" : ""
+          }
+          onChange={(e) => setText(e.target.value)}
+          onKeyPress={() => handleKeyPress()}
+          className={done ? "done" : "not-done"}
+        />
+      </div>
+    </div>
+  )
+}
